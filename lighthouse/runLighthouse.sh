26ab12e9339be1e44c3de9897f4fbeef9588393a
Addy Osmani <addyosmani@gmail.com>
2018-04-20 16:11:07 -0700

    Fix offline-games route in LH script

--- lighthouse/runLighthouse.sh
+++ lighthouse/runLighthouse.sh
@@ -6,4 +6,4 @@ mkdir -p $REPORTS_DIR/$LAST_COMMIT &&
 cd $REPORTS_DIR/$LAST_COMMIT &&
 lighthouse http://localhost:8080 --perf --output=json --output-path=root.json &&
 lighthouse http://localhost:8080/#/browse --perf --output=json --output-path=browse.json &&
-lighthouse http://localhost:8080/#/offline --perf --output=json --output-path=offline.json
+lighthouse http://localhost:8080/#/offline-games --perf --output=json --output-path=offline.json
